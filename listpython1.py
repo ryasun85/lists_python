7c838d636485328826d4a44289b388af5d3293e4
ryasun85 <59563060+ryasun85@users.noreply.github.com>
2020-01-21 15:11:59 +0000

    Add files via upload

---
+++ listpython1.py
@@ -0,0 +1,29 @@
+# favourite_songs=[
+#     "Phillip George- wish you were mine",
+#     "Amay Winehouse - Valerie",
+#     "Synapson - All in you"
+# ]
+# print(favourite_songs)
+# print(favourite_songs[2])
+
+favourite_songs=[
+    "Phillip George- wish you were mine",
+    "Amay Winehouse - Valerie",
+    "Synapson - All in you"
+]
+print(favourite_songs)
+print(favourite_songs[2])
+
+favourite_songs[2] = "Waze and odysay - bump & grind"
+print(favourite_songs)
+
+print(len(favourite_songs))
+
+favourite_songs.append("shakespears Sisters- stay")
+
+print(favourite_songs)
+
+favourite_songs.pop()
+
+print(favourite_songs)
+
---
+++ listpythonactivities2.py
@@ -0,0 +1,35 @@
+#Task 1 start
+
+# favourite_websites = [
+#     "ebay",
+#     "facebook"
+#     "skyscanner"
+# ]
+
+# favourite_websites.append("manchester confidential")
+# favourite_websites.append("google")
+
+# print(favourite_websites)
+
+# favourite_websites.pop()
+
+# print(favourite_websites)
+
+# Task 1 end
+
+
+favourite_websites = [
+    "ebay",
+    "facebook"
+    "skyscanner"
+]
+
+favourite_websites.extend("manchester confidential", "google")
+
+print(favourite_websites)
+
+favourite_websites.pop()
+
+print(favourite_websites)
+
+
